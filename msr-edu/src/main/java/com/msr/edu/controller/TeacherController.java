66ed87e070800ad73c4edd72b7008bf180dc8367
mengfei923300535 <mengfei923300535@gmail.com>
2020-06-12 19:51:26 +0900

    搭建项目框架

--- msr-edu/src/main/java/com/msr/edu/controller/TeacherController.java
+++ msr-edu/src/main/java/com/msr/edu/controller/TeacherController.java
@@ -4,7 +4,10 @@ package com.msr.edu.controller;
 import com.msr.edu.entity.Teacher;
 import com.msr.edu.service.TeacherService;
 import com.msr.edu.service.impl.TeacherServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,12 +23,15 @@ import java.util.List;
  * @author tom
  * @since 2020-06-12
  */
+@Api(description="讲师管理")
+@CrossOrigin //跨域
 @RestController
 @RequestMapping("/edu/teacher")
 public class TeacherController {
     @Autowired
     private TeacherService teacherService;
 
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping
     public List<Teacher> list(){
         return teacherService.list(null);
